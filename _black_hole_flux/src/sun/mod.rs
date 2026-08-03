ddb885644793e16a0f68cd8ebae8a31845ee45e8
The Man in the Box <mitbagent@gmail.com>
2026-08-03 01:46:33 -0700

    chore: Address comment 11156

--- _black_hole_flux/src/sun/mod.rs
+++ _black_hole_flux/src/sun/mod.rs
@@ -1,11 +1,34 @@
-use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::collections::{HashMap, HashSet};
+use std::marker::PhantomData;
 
+use black_hole_spec::ObjectId;
+use jungle_sdk::prelude::*;
 use uuid::Uuid;
 
-pub struct SunState(Arc<Mutex<SunInner>>);
-pub struct SunInner {
+// 1. Spawn all children getting uuids
+// OUTER LOOP
+// // FOR STAGES (prop1, prop2, potentiation)
+// // 2. Build topological ordering
+// // // WHILE TOPO NOT EMPTY
+// // // 3. Pop topo vec into current
+// // // // WHILE CURRENT NOT EMPTY
+// // // // 4. wait for FIRST rx of the set
+// // // // 5. remove from current and rotate rx
+// // // // 6. construct and send to outgoing-tx with rx ObjectIds as send
+// // // // 7. rotate tx
+
+pub struct Tag<N, T, E>(PhantomData<N>, PhantomData<T>, PhantomData<E>);
+
+pub struct SunState {
+    incoming: HashMap<Uuid, Vec<Uuid>>,
     outgoing: HashMap<Uuid, Vec<Uuid>>,
     tx: HashMap<Uuid, ObjectId>,
     rx: HashMap<Uuid, ObjectId>,
+    topo: Vec<HashSet<Uuid>>,
+    current: HashSet<Uuid>,
 }
+
+pub struct Sun<Cells>(PhantomData<Cells>);
+
+#[derive(Flow)]
+pub struct Ray<T, U>(Step<Spawn<T>>, U);
